a32b69049de8a504e0327d5432cd0c7d6176964b
Michael Trew <40902786+mtrew2015@users.noreply.github.com>
2020-01-04 04:40:53 -0500

    quick edit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.scss';
 import Header from './components/Header';
 import Calculator from './components/Calculator';
 
-
 function App() {
-
 	return (
-		<div className="container">
-			<div className="App" />
-			<Header/>
-			<Calculator/>
+		<div className="wrapper">
+			<div className="container">
+				<div className="App" />
+				<Header />
+				<Calculator />
+			</div>
 		</div>
 	);
 }
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,78 +4,66 @@ import '../App.scss';
 import math from '../math';
 
 const Calculator = (props) => {
-	const [ mathString, setMathString ] = useState('');
-	const [ result, setResult ] = useState(0);
-	const [ current, setCurrent ] = useState('0');
 	const [ display, setDisplay ] = useState('');
-	const [ lastOp, setLastOp ] = useState('');
-	const [ lastNum, setLastNum ] = useState('0');
 
-	const operationHandler = (e) => {
-		const operator = e.target.value;
-
-	};
-
-	function addToString(e){
+	function addToString(e) {
 		let value = e.target.value;
-		if (value === "="){
-			setDisplay(math.eval(display))
-		} else{
-			setDisplay((prev) => prev + value)
+		if (value === '=') {
+			setDisplay(math.eval(display));
+		} else {
+			setDisplay((prev) => prev + value);
 		}
-		
 	}
 
 	const specialClick = (e) => {
 		const val = e.target.value;
-		if(val === "C"){
-			setDisplay('')
-		} else if (val === "+/-") {
-			if(display.slice(0) === "-"){
-				setDisplay(prev => prev.substr(1))
+		if (val === 'C') {
+			setDisplay('');
+		} else if (val === '+/-') {
+			if (display.slice(0) === '-') {
+				setDisplay((prev) => prev.substr(1));
 			} else {
-				setDisplay(prev => "-" + prev )
+				setDisplay((prev) => '-' + prev);
 			}
-		} else{
-			if(val === "%"){
-				setDisplay(prev => math.evaluate(`${prev} / 100`))
+		} else {
+			if (val === '%') {
+				setDisplay((prev) => math.evaluate(`${prev} / 100`));
 			}
-		} 
+		}
 	};
 
 	// STEP 2 - add the imported data to state
 	return (
 		<div className="container">
-			
 			<div className="display">{display}</div>;
 			<div className="buttonsContainer">
-			<section className="col-1"> 
-				{specials.map((special, idx) => {
-					return (
-						<button onClick={specialClick} value={special} id="specials" className="button">
-							{special}
-						</button>
-					);
-				})}
-				{numbers.map((num, idx) => {
-					return (
-						<button onClick={addToString} value={num} className="button">
-							{num}
-						</button>
-					);
-				})}
-			</section>
-			<section className="col-2">
-			<div>
-				{operators.map((operator, idx) => {
-					return (
-						<button onClick={addToString} value={operator.value} className="opButton">
-							{operator.char}
-						</button>
-					);
-				})}
-			</div>
-			</section>
+				<section className="col-1">
+					{specials.map((special, idx) => {
+						return (
+							<button onClick={specialClick} value={special} id="specials" className="button">
+								{special}
+							</button>
+						);
+					})}
+					{numbers.map((num, idx) => {
+						return (
+							<button onClick={addToString} value={num} className="button">
+								{num}
+							</button>
+						);
+					})}
+				</section>
+				<section className="col-2">
+					<div>
+						{operators.map((operator, idx) => {
+							return (
+								<button onClick={addToString} value={operator.value} className="opButton">
+									{operator.char}
+								</button>
+							);
+						})}
+					</div>
+				</section>
 			</div>
 		</div>
 	);
